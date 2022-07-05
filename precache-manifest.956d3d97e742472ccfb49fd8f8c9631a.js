cd4b83a5fbc20037d643d25640fce63005c82dd3
Sudhanshu Kumar <sudhanshu.kumar@prohpel.com>
2022-07-05 14:57:13 +0530

    Updates

---
+++ precache-manifest.956d3d97e742472ccfb49fd8f8c9631a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "09969accc61a5898be18",
+    "url": "/email-signature-prohpel/static/css/main.de641bb2.chunk.css"
+  },
+  {
+    "revision": "09969accc61a5898be18",
+    "url": "/email-signature-prohpel/static/js/main.09969acc.chunk.js"
+  },
+  {
+    "revision": "ec4bf9c6b30ae6d60de6",
+    "url": "/email-signature-prohpel/static/js/1.ec4bf9c6.chunk.js"
+  },
+  {
+    "revision": "a19de1a2d91e04697b84",
+    "url": "/email-signature-prohpel/static/js/runtime~main.a19de1a2.js"
+  },
+  {
+    "revision": "1f742d0a34095d37d528348794ca5aa6",
+    "url": "/email-signature-prohpel/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/email-signature-prohpel/precache-manifest.956d3d97e742472ccfb49fd8f8c9631a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/email-signature-prohpel/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
